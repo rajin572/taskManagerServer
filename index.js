5ffbb3c9a5bf500b14f3ddc71b400c7df7c44141
Din Islam Rajin <dirrazin572@gmail.com>
2022-12-29 17:37:41 +0600

    added the database and create some route

--- index.js
+++ index.js
@@ -17,24 +17,51 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nl9uncn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 
-// async function run() {
-//     try {
-//         const serviceCollection = client.db("taskManagerServer").collection("services");
-//         const reviewCollection = client.db("upturn").collection("reviews");
+async function run() {
+    try {
+        const myTaskCollection = client.db("taskManagerServer").collection("myTask");
+        const completeTaskCollection = client.db("taskManagerServer").collection("completeTask");
 
 
-//     } finally {
+        app.get('/task', async (req, res) => {
+            const query = {}
+            const cursor = myTaskCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
 
-//     }
-// }
-// run().catch(error => console.error(error))
+        //to post new service on the website and database
+        app.post('/task', async (req, res) => {
+            const order = req.body;
+            const result = await myTaskCollection.insertOne(order)
+            res.send(result)
+            console.log(result);
+        })
+
+        app.get('/myTask', async (req, res) => {
+            let query = {}
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = myTaskCollection.find(query)
+            const mytask = await cursor.toArray()
+            res.send(mytask)
+        })
+
+    } finally {
+
+    }
+}
+run().catch(error => console.error(error))
 
 
 
